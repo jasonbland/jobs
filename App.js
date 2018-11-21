cb4737e8e245c2060dfaebefaa61bb287be52a21
Jason Bland <jtbland@gmail.com>
2018-11-20 18:44:12 -0800

    add tab navigator

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = createBottomTabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen }
+    });
+
+    const App = createAppContainer(MainNavigator);
+
     return (
       <View style={styles.container}>
-        <Text>Jobs!</Text>
+        <App />
       </View>
     );
   }
