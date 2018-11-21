0a05cf796b323191328f720f516928ffe3cef08f
Jason Bland <jtbland@gmail.com>
2018-11-20 19:27:37 -0800

    add navigation options to review screen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  static navigationOptions = {
+    title: 'Review Jobs',
+    headerRight: <Button title="Settings" onPress={() => {}} />
+  };
+
   render() {
     return (
       <View>
