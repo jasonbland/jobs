7d6aaec8a4d2145ff65269d072630d49d2ccb9af
Jason Bland <jtbland@gmail.com>
2018-11-20 22:52:13 -0800

    add button to last slide on oncomplete navigate method

--- components/Slides.js
+++ components/Slides.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { Dimensions, ScrollView, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+      return <Button buttonStyle={styles.buttonStyle} title="Enter" raised onPress={this.props.onComplete} />;
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map(slide => {
+    return this.props.data.map((slide, index) => {
       return (
         <View style={[styles.slideStyle, { backgroundColor: slide.color }]} key={slide.text}>
           <Text style={styles.textStyle}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
         </View>
       );
     });
@@ -32,7 +40,12 @@ const styles = {
   },
   textStyle: {
     fontSize: 30,
-    color: 'white'
+    color: 'white',
+    textAlign: 'center'
+  },
+  buttonStyle: {
+    backgroundColor: '#0288D1',
+    marginTop: 15
   }
 };
 
