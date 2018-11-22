b835b6228679e93cb033f7c77b9b0ee02c511fd5
Jason Bland <jtbland@gmail.com>
2018-11-21 22:24:53 -0800

    fix action typo

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,7 +1,7 @@
 import { AsyncStorage } from 'react-native';
 import { Facebook } from 'expo';
 import { FACEBOOK_APPID } from 'react-native-dotenv';
-import { FACEBOK_LOGIN_FAIL, FACEBOOK_LOGIN_SUCCESS } from './types';
+import { FACEBOOK_LOGIN_FAIL, FACEBOOK_LOGIN_SUCCESS } from './types';
 
 export const facebookLogin = () => async dispatch => {
   let token = await AsyncStorage.getItem('fb_token');
--- actions/types.js
+++ actions/types.js
@@ -1,2 +1,2 @@
-export const FACEBOK_LOGIN_SUCCESS = 'facebook_login_success';
-export const FACEBOK_LOGIN_FAIL = 'facebook_login_fail';
+export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
+export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
