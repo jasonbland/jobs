4fc7c67107623f34e87d6e14ea622b0fe83b7e8b
Jason Bland <jtbland@gmail.com>
2018-11-21 20:52:35 -0800

    add auth action with es6 no return statement function

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,7 +1,12 @@
 import { AsynchStorage } from 'react-native';
 import { FACEBOOK_LOGIN_SUCCESS } from './types';
 
-export const facebookLogin = () => {
-  //   AsynchStorage.setItem('fb_token', token);
-  //   AsynchStorage.getItem('fb_token');
+export const facebookLogin = () => async dispatch => {
+  let token = await AsynchStorage.getItem('fb_token');
+
+  if (token) {
+    // dispatch an action that fb login is done
+  } else {
+    // start up fb login steps
+  }
 };
