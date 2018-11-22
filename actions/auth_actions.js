e515190a171a33d1273ce7844ad441f35084cb43
Jason Bland <jtbland@gmail.com>
2018-11-21 21:20:32 -0800

    add facebook auth helper function, set item, and dispatch

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,12 +1,31 @@
 import { AsynchStorage } from 'react-native';
-import { FACEBOOK_LOGIN_SUCCESS } from './types';
+import { Facebook } from 'expo';
+import dotenv from 'dotenv';
+import { FACEBOK_LOGIN_FAIL, FACEBOOK_LOGIN_SUCCESS } from './types';
+
+dotenv.config();
 
 export const facebookLogin = () => async dispatch => {
   let token = await AsynchStorage.getItem('fb_token');
 
   if (token) {
     // dispatch an action that fb login is done
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
   } else {
-    // start up fb login steps
+    doFacebookLogin(dispatch);
+    // start up fb login step
   }
+};
+
+const doFacebookLogin = async dispatch => {
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync(process.env.FACEBOOK_APPID, {
+    permissions: ['public_profile']
+  });
+
+  if (type === 'cancel') {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+  }
+
+  await AsynchStorage.setItem('fb_token', token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
 };
--- actions/types.js
+++ actions/types.js
@@ -1,1 +1,2 @@
 export const FACEBOK_LOGIN_SUCCESS = 'facebook_login_success';
+export const FACEBOK_LOGIN_FAIL = 'facebook_login_fail';
