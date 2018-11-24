9104681e10647e3f520e534ac0a86c1a171b520f
Jason Bland <jtbland@gmail.com>
2018-11-24 01:21:47 -0800

    add mapview from expo

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
       </View>
     );
   }
