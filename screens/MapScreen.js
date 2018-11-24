4e40cf83321a81834bfc6ff97922e74d01a70ee7
Jason Bland <jtbland@gmail.com>
2018-11-24 11:52:21 -0800

    add mapview and default region state

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,18 +3,23 @@ import { Text, View } from 'react-native';
 import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    region: {
+      latitude: 37,
+      longitude: -122,
+      latitudeDelta: 0.09,
+      longitudeDelta: 0.04
+    }
+  };
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <MapView
-          style={{ flex: 1 }}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }}
-        />
+        <MapView style={{ flex: 1 }} initialRegion={this.state.region} onRegionChange={this.onRegionChange} />
       </View>
     );
   }
