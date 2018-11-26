d1da7c9f7a89222723fb5ee2db3092fd6c3fac52
Jason Bland <jtbland@gmail.com>
2018-11-25 21:30:35 -0800

    add render card function

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { MapView } from 'expo';
+import { Button, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 
 import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    return (
+      <Card title={job.jobtitle}>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text>{job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}</Text>
+      </Card>
+    );
+  }
+
   render() {
     return (
       <View>
-        <Swipe data={this.props.jobs} />
+        <Swipe data={this.props.jobs} renderCard={this.renderCard} />
       </View>
-    )
+    );
+  }
 }
+
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
+  }
+};
 
 function mapStateToProps({ jobs }) {
   return { jobs: jobs.results };
