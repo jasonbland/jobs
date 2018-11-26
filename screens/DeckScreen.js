b439e6c2c60a2a1dba6c38e6612adeec5711fe6a
Jason Bland <jtbland@gmail.com>
2018-11-25 21:44:31 -0800

    add map view to each card with job location

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Platform, Text, View } from 'react-native';
 import { MapView } from 'expo';
 import { Button, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
@@ -8,21 +8,42 @@ import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
   renderCard(job) {
+    const initialRegion = {
+      latitude: job.latitude,
+      longitude: job.longitude,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.02
+    };
+
     return (
       <Card title={job.jobtitle}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={Platform.OS === 'android' ? true : false}
+            initialRegion={initialRegion}
+          />
+        </View>
+
         <View style={styles.detailWrapper}>
           <Text>{job.company}</Text>
           <Text>{job.formattedRelativeTime}</Text>
         </View>
+
         <Text>{job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}</Text>
       </Card>
     );
   }
 
+  renderNoMoreCards() {
+    return <Card title="No more jobs" />;
+  }
+
   render() {
     return (
       <View>
-        <Swipe data={this.props.jobs} renderCard={this.renderCard} />
+        <Swipe data={this.props.jobs} renderCard={this.renderCard} renderNoMoreCards={this.renderNoMoreCards} />
       </View>
     );
   }
