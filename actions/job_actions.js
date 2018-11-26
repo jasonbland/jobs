600dc72c828e630620d10dd9f874db0cf81abdd4
Jason Bland <jtbland@gmail.com>
2018-11-25 20:51:37 -0800

    add callback to navigate after fetching jobs

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -19,12 +19,14 @@ const buildJobsUrl = zip => {
   return `${JOB_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = region => async dispatch => {
+export const fetchJobs = (region, callback) => async dispatch => {
   try {
     let zip = await Location.reverseGeocodeAsync(region);
     const url = buildJobsUrl(zip);
     let { data } = await axios.get(url);
+
     dispatch({ type: FETCH_JOBS, payload: data });
+    callback();
   } catch (e) {
     console.error(e);
   }
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -28,7 +28,9 @@ class MapScreen extends Component {
   };
 
   onButtonPress = () => {
-    this.props.fetchJobs(this.state.region);
+    this.props.fetchJobs(this.state.region, () => {
+      this.props.navigation.navigate('deck');
+    });
   };
 
   render() {
