0463325d2784f1dda422f0d92ca136a781f16fe9
Jason Bland <jtbland@gmail.com>
2018-11-25 17:19:42 -0800

    add build url helper, axios request, and dispatch

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -4,6 +4,7 @@ import qs from 'qs';
 
 import { FETCH_JOBS } from './types';
 
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
 const JOB_QUERY_PARAMS = {
   publisher: '2377831494416694',
   format: 'json',
@@ -13,9 +14,18 @@ const JOB_QUERY_PARAMS = {
   q: 'javascript'
 };
 
+const buildJobsUrl = zip => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
 export const fetchJobs = region => async dispatch => {
   try {
     let zip = await reverseGeoCode(region);
+    const url = buildJobsUrl(zip);
+    let { data } = await axios.get(url);
+    dispatch({ type: FETCH_JOBS, payload: data });
+    console.log(data);
   } catch (e) {
     console.error(e);
   }
