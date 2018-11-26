8a27fcb3538fbb9315d1b68b7aec2fcfdb124594
Jason Bland <jtbland@gmail.com>
2018-11-25 17:08:44 -0800

    add reversegeocode and qs and query params

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
+import reverseGeoCode from 'latlng-to-zip';
+import qs from 'qs';
 
 import { FETCH_JOBS } from './types';
 
-export const fetchJobs = region => async dispatch => {};
+const JOB_QUERY_PARAMS = {
+  publisher: '2377831494416694',
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'javascript'
+};
+
+export const fetchJobs = region => async dispatch => {
+  try {
+    let zip = await reverseGeoCode(region);
+  } catch (e) {
+    console.error(e);
+  }
+};
