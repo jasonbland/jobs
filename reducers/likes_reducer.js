3ae9437d978f16f1b5eab71125333771b2596984
Jason Bland <jtbland@gmail.com>
2018-11-26 20:55:10 -0800

    add like job state return and default

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,9 +1,10 @@
+import _ from 'lodash';
 import { LIKE_JOB } from '../actions/types';
 
-export default function(state, action) {
+export default function(state = [], action) {
   switch (action.type) {
     case LIKE_JOB:
-      return action.payload;
+      return _.uniqBy([action.payload, ...state], 'jobkey');
     default:
       return state;
   }
