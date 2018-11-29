1ebc6096f6de41690b57423f87391ad382f38498
Jason Bland <jtbland@gmail.com>
2018-11-28 22:35:09 -0800

    add rehydrate to likes reducer for likedjobs

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
+import { REHYDRATE } from 'redux-persist/constants';
 import { CLEAR_LIKES, LIKE_JOB } from '../actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
+    case REHYDRATE:
+      return action.payload.likedJobs;
     case LIKE_JOB:
       return _.uniqBy([action.payload, ...state], 'jobkey');
     case CLEAR_LIKES:
