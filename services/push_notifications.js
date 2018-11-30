e7c8dbdecdbc7e3ccee384ae3a328f95c4bbe2a9
Jason Bland <jtbland@gmail.com>
2018-11-29 21:55:35 -0800

    add notification permissions and token

--- services/push_notifications.js
+++ services/push_notifications.js
@@ -3,4 +3,16 @@ import { AsyncStorage } from 'react-native';
 
 export default async () => {
   let previousToken = await AsyncStorage.getItem('pushtoken');
+
+  if (previousToken) {
+    return;
+  } else {
+    let { status } = await Permissions.askAsync(Permissions.REMOTE_NOTIFICATIONS);
+
+    if (status !== 'granted') {
+      return;
+    }
+
+    let token = await Notifications.getExpoPushTokenAsync();
+  }
 };
