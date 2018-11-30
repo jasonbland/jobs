87118b3c0de3969ac9615147b5746813d2aa62f8
Jason Bland <jtbland@gmail.com>
2018-11-29 22:13:04 -0800

    request token and store in local storage

--- services/push_notifications.js
+++ services/push_notifications.js
@@ -1,5 +1,8 @@
 import { Permissions, Notifications } from 'expo';
 import { AsyncStorage } from 'react-native';
+import axios from 'axios';
+
+const PUSH_ENDPOINT = 'http://rallycoding.herokuapp.com/api/tokens';
 
 export default async () => {
   let previousToken = await AsyncStorage.getItem('pushtoken');
@@ -14,5 +17,11 @@ export default async () => {
     }
 
     let token = await Notifications.getExpoPushTokenAsync();
+
+    await axios.post(PUSH_ENDPOINT, { token: { token } });
+    AsyncStorage.setItem('pushtoken', token);
   }
+
+  // use this as a back end push server temporarily
+  // http://rallycoding.herokuapp.com/api/tokens
 };
